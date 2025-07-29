9d9e5a8353761cef1a518458dea0ea477a2b6921
sabbir-063 <202114063@student.mist.ac.bd>
2025-07-29 23:02:26 +0600

    env updated for both development and production

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,24 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API_URL,
+    // headers: {
+    //     "Content-Type": "application/json",
+    //     "Access-Control-Allow-Origin": "*",
+    // },
+    // withCredentials: true, // Include credentials for cross-origin requests
+    // timeout: 10000, // Set a timeout for requests
+    // responseType: "json", // Expect JSON responses
+    // validateStatus: (status) => {
+    //     // Accept all status codes
+    //     return status >= 200 && status < 600;
+    // },
+    // maxRedirects: 5, // Allow up to 5 redirects
+    // paramsSerializer: (params) => {
+    //     // Serialize query parameters
+    //     return new URLSearchParams(params).toString();
+    // },
+
 });
 
 // Add request interceptor to add token to headers
