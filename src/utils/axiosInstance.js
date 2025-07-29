aeb97fc4f279691c948257ec626f16c07151109d
Mohammad Sabbir Musfique <114491259+sabbir-063@users.noreply.github.com>
2025-07-29 21:10:05 +0600

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://blogsite-backend-4fmp.onrender.com/api",
 });
 
 // Add request interceptor to add token to headers
